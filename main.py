eeed4efbc55620aed38b8735f06f3812dc2b632b
sdual <shr.nrht.0623@gmail.com>
2023-01-20 02:41:10 +0900

    fix port

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ def run_api():
 
 if __name__ == '__main__':
     try:
-        uvicorn.run(run_api, port=5000, log_level='info')  # uvicorn 以外の web server 使ってる場合はここは書き換える
+        uvicorn.run(run_api, port=8000, log_level='info')  # uvicorn 以外の web server 使ってる場合はここは書き換える
         # FastAPI での logger の設定がどうであれ、それに関係なく web server 側に渡されてしまうらしい。
         # logger の設定は web server 側で行い、例えば uvicorn の場合は
         # uvicorn.run(app, host="0.0.0.0", port=5000, log_config=[設定ファイルへのpath])
